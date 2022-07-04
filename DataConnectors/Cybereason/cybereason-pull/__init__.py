9c79cc816709ee2ace445bb45005fdddaba49aa5
deepanshumarwah <55777311+deepanshumarwah@users.noreply.github.com>
2022-07-04 09:18:18 +0800

    Update DataConnectors/Cybereason/cybereason-pull/__init__.py
    Co-authored-by: Anthony Ledesma <aledesma@mindstream.it>

--- DataConnectors/Cybereason/cybereason-pull/__init__.py
+++ DataConnectors/Cybereason/cybereason-pull/__init__.py
@@ -106,7 +106,7 @@ def generate_date():
 
 def build_signature(customer_id, shared_key, date, content_length, method, content_type, resource):
     x_headers = 'x-ms-date:' + date
-    string_to_hash = method + "\n" + str(content_length) + "\n" + content_type + "\n" + x_headers + "\n" + resource
+    string_to_hash = f"{method}\n{content_length}\n{content_type}\n{x_headers}\n{resource}"
     bytes_to_hash = bytes(string_to_hash, encoding="utf-8")
     decoded_key = base64.b64decode(shared_key)
     encoded_hash = base64.b64encode(hmac.new(decoded_key, bytes_to_hash, digestmod=hashlib.sha256).digest()).decode()
